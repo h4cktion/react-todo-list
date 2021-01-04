ebdec336172796d82d7d3b8943446d5edb4b1356
hacktion <matthieu.wandolski@gmail.com>
2021-01-04 11:48:41 +0100

    todo list using react hooks

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import useStyles from '../styles/AppStyles';
+import TodoApp from './TodoApp';
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.App}>
+      <header className={classes.header}>
+        <h1>
+          todo<span>list</span>
+        </h1>
+        <h2>A simple todolist app built with React Hooks & Context</h2>
+      </header>
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/TodoFormStyles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+  TodoForm: {
+/*     margin: '50px auto',
+    width: '25%', */
+    marginBottom: '1rem'
+  },
+  input: {
+    width: '100%',
+    height: '2rem',
+    background: 'none',
+    border: 'none',
+    borderBottom: '1px solid #34495e',
+    color: '#34495e',
+    fontSize: '1.2rem',
+    fontWeight: '300',
+    outline: 'none'
+  }
+});
